51deecb437213f51599b19b8b4fa8cf55d2b38fc
Zoë <s1107714@student.hsleiden.nl>
2021-04-07 15:04:48 +0200

    Animal routes organized and styled

--- IATDB_PassenOpJeDier/resources/views/animal/components/animalCard--index.blade.php
+++ IATDB_PassenOpJeDier/resources/views/animal/components/animalCard--index.blade.php
@@ -3,18 +3,16 @@
         <article >
             <header class="gridCard__header u-flex-v-center">
                 <h2 class="gridCard__heading">{{$animal->name}}</h2>
-                <h4 class="gridCard__subheading">{{$animal->species}} </h4>
-                <h4 class="gridCard__subheading">{{$animal->id}} </h4>
 
             </header>
-            <!-- <figure class="gridCard__figure">
-                <img class="gridCard__image" src="{{$animal->image}}" alt="{{$animal->name . ' ' . $animal->species}}">
-            </figure> -->
-            <!-- <section class="gridCard__textSection u-flex-v-center">
-                <p class="gridCard__text">
-                    {{$animal->description}}
-                </p>   
-            </section> -->
+            <figure class="gridCard__figure u-flex-v-center" >
+                <img class="gridCard__image u-flex-v-center" src="{{$animal->image}}" alt="{{$animal->name . ' ' . $animal->species}}">
+            </figure>
+  
+            <section class="gridCard__buttonSection u-flex-v-center">
+                <button class="gridCard__button u-flex-v-center" onclick="/animal/{{$animal->id}}">Meer over {{$animal->name}}</button>
+            </section>
+
         </article>
     </a>
 </li>
--- IATDB_PassenOpJeDier/resources/views/animal/components/animalCard--show.blade.php
+++ IATDB_PassenOpJeDier/resources/views/animal/components/animalCard--show.blade.php
@@ -1,16 +1,19 @@
-<article class="animalCard a-popup" data-species-of-animal={{$animals->species}}>
-    <header class="gridCard__header u-flex-v-center">
-                <h2 class="gridCard__heading">{{$animals->name}}</h2>
-                <h4 class="gridCard__subheading">{{$animals->species}} </h4>
-                <h4 class="gridCard__subheading">{{$animals->id}} </h4>
-
+<article class="showCard  a-popup" data-species-of-animal={{$animals->species}}>
+    <header class="showCard__header u-flex-v-center">
+                <h2 class="showCard__heading">{{$animals->name}}</h2>
         </header>
-        <figure class="animalCard__figure">
-            <img class="animalCard__image" src="{{$animals->image}}" alt="{{$animals->name . ' ' . $animals->species}}">
+        <figure class="showCard__figure u-flex-v-center">
+            <img class="showCard__image" src="{{$animals->image}}" alt="{{$animals->name . ' ' . $animals->species}}">
         </figure>
-        <section class="animalCard__text">
+        <section class="showCard__text u-flex-v-center">
             <p>
                 {{$animals->description}}
             </p>
         </section>
+
+        <section class="showCard__buttonSection u-flex-v-center">
+            <button class="showCard__button showCard__button-pick u-flex-v-center" onclick="">Op {{$animals->name}} passen</button>
+            <button class="showCard__button showCard__button-back u-flex-v-center" onclick="window.location.href='/animals'">Terug naar overzicht</button>
+        </section>
+
 </article>
